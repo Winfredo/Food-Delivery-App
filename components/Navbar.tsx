c0d335c3607d802d449afa0c4a4be85085ecc623
winfredo <winfrednukpe2002@gmail.com>
2026-01-22 18:46:54 +0000

    adding content to the navbar page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,20 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div>
-      <p>This is the Navbar.</p>
-      <Image src="/assets/logo.png" alt="Logo" width={100} height={100} />
+      <Image src="/assets/logo.png" alt="Logo" width={200} height={200}  />
+
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+         <li>Contact Us</li>
+      </ul>
+
+      <div>
+        <Image src="/assets/search_icon.png" alt="Search" width={30} height={30} />
+        <Image src="/assets/basket_icon.png" alt="Cart" width={30} height={30} />
+        <button className='bg-red-200'>Login</button>
+      </div>
     </div>
   )
 }
