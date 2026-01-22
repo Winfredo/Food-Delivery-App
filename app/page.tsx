6d9e3cfa50cd8a814fb6e288157d7e046d44eff8
winfredo <winfrednukpe2002@gmail.com>
2026-01-22 14:07:33 +0000

    adding assets to the file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Navbar from '@/components/Navbar'
 const page = () => {
   return (
     <div>
-      <p>This is the entry page.</p>
+      <Navbar />
+      
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div>
       <p>This is the Navbar.</p>
+      <Image src="/assets/logo.png" alt="Logo" width={100} height={100} />
     </div>
   )
 }
