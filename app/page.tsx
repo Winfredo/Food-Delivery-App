010a4e77c8c35a1c351df13c9a54523cc466c2a8
winfredo <winfrednukpe2002@gmail.com>
2026-01-29 18:38:28 +0000

    working on the navbar.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 const page = () => {
   return (
-    <div>
-      <Navbar />
-      
-    </div>
+   <div>
+    <Navbar />
+   </div>
   )
 }
 
