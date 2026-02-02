d14512830ef5bb8a7ae3d0f97374fe1fd8b1272d
winfredo <winfrednukpe2002@gmail.com>
2026-02-02 16:39:28 +0000

    creating the menu page and the pictures.

---
+++ constants/constants.ts
@@ -0,0 +1,35 @@
+import { MenuType } from "@/types/type"
+
+export const menuList: MenuType[] = [
+    {
+       name: "Salad",
+       image: "/assets/menu_1.png"
+    },
+    {
+       name: "Rolls",
+       image: "/assets/menu_2.png"
+    },
+    {
+       name: "Deserts",
+       image: "/assets/menu_3.png"
+    },
+    {
+       name: "Sandwich",
+       image: "/assets/menu_4.png"
+    },
+    {
+       name: "Cake",
+       image: "/assets/menu_5.png"
+    },
+    {
+       name: "Pure Veg",
+       image: "/assets/menu_6.png"
+    },
+    {
+       name: "Pasta",
+       image: "/assets/menu_7.png"
+    },
+    {
+       name: "Noodles",
+       image: "/assets/menu_8.png"
+    }]
