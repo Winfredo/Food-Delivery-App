aa4808514e05b7d4cd71708c721455525a7c9f1c
winfredo <winfrednukpe2002@gmail.com>
2026-02-02 16:40:18 +0000

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import Navbar from "@/components/Navbar";
+"use client"
+import React,{useState} from "react";
 import Header from "@/components/Header";
+import Exploremenu from "@/components/Exploremenu";
+
+
 const page = () => {
+  const [category, setCategory] = useState("All");
+
   return (
-    <div className="w-[80%] mx-auto">
-      <Navbar />
+    <div>
       <Header />
+      <Exploremenu category={category} setCategory={setCategory} />
+
     </div>
   );
 };
