14b577f7f4df73f1504c008e55594b44fefb9e73
winfredo <winfrednukpe2002@gmail.com>
2026-02-03 19:13:14 +0000

    types for typescript.

--- types/type.ts
+++ types/type.ts
@@ -9,3 +9,17 @@ export type Props = {
     category?: string
     setCategory?: React.Dispatch<React.SetStateAction<string>>
 }
+
+export type FoodType = {
+    _id?: string
+    name?: string
+    rating?: string
+    image?: string
+    price?: number
+    description?: string
+    category?: string
+}
+
+export type StoreContextValue = {
+  foodList: FoodType[]
+}
