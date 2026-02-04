158117c416f8ba8c6a5e01edb7a8953499da63f4
winfredo <winfrednukpe2002@gmail.com>
2026-02-04 18:26:54 +0000

    finish up the component.

--- components/FoodItem.tsx
+++ components/FoodItem.tsx
@@ -4,15 +4,18 @@ import { FoodType } from '@/types/type'
 import Image from 'next/image'
 const FoodItem = ({_id,name,image,price,description,category}: FoodType) => {
   return (
-    <div className='w-full m-auto rounded-[15px] shadow-[0px_0px_10px_#000015] animation-fadeIn  hover:shadow-[0px_0px_20px_#000015] cursor-pointer p-3'>
-      <div className=''>
-        <img src={image} alt={name} />
-      </div>
+    <div className='w-full m-auto rounded-[15px] shadow-[0px_0px_10px_#00000015] animation-fadeIn cursor-pointer'>
       <div>
-      <h3>{name}</h3>
-      <Image src="/assets/rating_starts.png" alt="rating" width={70} height={70} />
-      <p>{description}</p>
-      <div className='food-item-price'>${price}</div>
+        <img className='w-full rounded-t-[15px]' src={image} alt={name} />
+      </div>
+      <div className='p-3'>
+        <div className='flex justify-between items-center mb-2.5'>
+      <h3 className='font-bold text-[17px]'>{name}</h3>
+      <Image className='w-16' src="/assets/rating_starts.png" alt="rating" width={70} height={70} />
+
+        </div>
+      <p className='text-[12px] text-[#676767]'>{description}</p>
+      <div className='text-[18px] my-2 font-semibold text-[#FF6347]'>${price}</div>
       <p>{category}</p>
 
       </div>
