84067465d189a1d04b5bb867f24d50093932984c
winfredo <winfrednukpe2002@gmail.com>
2026-02-05 18:53:18 +0000

    add the removeCart and addCart function.

--- context/StoreContextProvider.tsx
+++ context/StoreContextProvider.tsx
@@ -1,23 +1,49 @@
 "use client";
-import React,{createContext} from 'react'
-import {foodList} from "@/constants/constants"
-export const storeContext = createContext<StoreContextType | null>(null)
-type StoreContextType = {
-    foodList: any[]; 
-}
-const StoreContextProvider = (props:any) => {
-    
-    const storeValue:StoreContextType = {
-         foodList: foodList
+import React, { createContext, useEffect, useState } from "react";
+import { foodList } from "@/constants/constants";
+import { CartItems } from "@/types/type";
+import { StoreContextType } from "@/types/type";
+
+export const storeContext = createContext<StoreContextType | null>(null);
+
+const StoreContextProvider = (props: any) => {
+  const [cartItems, setCartItems] = useState<CartItems>({});
+
+  const addToCart = (foodId: string) => {
+    if (!cartItems[foodId]) {
+      setCartItems((prev) => ({ ...prev, [foodId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [foodId]: prev[foodId] + 1 }));
     }
+  };
 
-    return (
-        <storeContext.Provider value={storeValue}>
-            {props.children}
-        </storeContext.Provider>
-    )
-}
+ const removeFromCart = (foodId: string) => {
+  setCartItems((prev) => {
+    if (prev[foodId] === 1) {
+      const newItems = { ...prev }
+      delete newItems[foodId] 
+      return newItems
+    }
+    return { ...prev, [foodId]: prev[foodId] - 1 }
+  })
+};
 
-export default StoreContextProvider
+useEffect(()=> {
+    console.log(cartItems)
+},[cartItems])
+  const storeValue: StoreContextType = {
+    foodList: foodList,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
 
+  return (
+    <storeContext.Provider value={storeValue}>
+      {props.children}
+    </storeContext.Provider>
+  );
+};
 
+export default StoreContextProvider;
