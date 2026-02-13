36d707bb207a246d11a86401e99cd0571e00c483
winfredo <winfrednukpe2002@gmail.com>
2026-02-13 22:03:46 +0000

    work in the component.

--- components/LoginPopup.tsx
+++ components/LoginPopup.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPopup = () => {
+
+    const [currentState, setCurrentState] = useState("Sign Up");
   return (
     <div className=''>
-      <p>LoginPopup</p>
+      <form>
+        <div>
+            <h2>{currentState}</h2>
+            <img src="/assets/cross_icon.png" alt="cross icon" />
+        </div>
+      </form>
     </div>
   )
 }
