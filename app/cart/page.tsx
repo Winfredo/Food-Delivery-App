90aecb5c3ee81ad621e2432707e412f72beb849d
winfredo <winfrednukpe2002@gmail.com>
2026-02-15 22:53:53 +0000

    add items to the cart page.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React,{useContext} from 'react'
 import {storeContext} from "@/context/StoreContextProvider";
 
@@ -5,8 +6,20 @@ const page = () => {
 
   const {cartItems,foodList, removeFromCart,addToCart} = useContext(storeContext)!
   return (
-    <div>
-      <p>cart page</p>
+    <div className='cart'>
+     <div className='cart-items'>
+      <div className='cart-items-title'>
+        <p>Items</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <br />
+     <hr className="my-2.5 mx-auto border-none bg-[#e2e2e2] h-0.5 w-[90%]" />
+
+     </div>
     </div>
   )
 }
