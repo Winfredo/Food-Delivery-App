226367f3387537ee4e4c8a981578cf04440e8f0f
winfredo <winfrednukpe2002@gmail.com>
2026-02-24 12:33:54 +0000

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,4 +4,3 @@ export const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI)
 
 }
-//mongoose.connect('mongodb+srv://winfred_db_user:sWRbm5OS0eB4IAMu@cluster0.vkyv9wk.mongodb.net/food-delivery-app'
