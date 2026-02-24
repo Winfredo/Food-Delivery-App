ec3480ebed77882322ad2af362a73bbf4d52ca0f
winfredo <winfrednukpe2002@gmail.com>
2026-02-24 12:25:30 +0000

    make changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://winfred_db_user:sWRbm5OS0eB4IAMu@cluster0.vkyv9wk.mongodb.net/food-delivery-app').then(()=> console.log('DB connected'))
+    await mongoose.connect(process.env.MONGODB_URI)
+
 }
+//mongoose.connect('mongodb+srv://winfred_db_user:sWRbm5OS0eB4IAMu@cluster0.vkyv9wk.mongodb.net/food-delivery-app'
