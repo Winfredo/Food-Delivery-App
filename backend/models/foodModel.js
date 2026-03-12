5b6a436a76886003fe0d39cf2b926c1a3a4da813
winfredo <winfrednukpe2002@gmail.com>
2026-03-12 21:54:41 +0000

    fixes on the food schema

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -4,11 +4,11 @@ const foodSchema = new mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
-    image: {type: String, required: true},
+    image: {type: String, required: false},
     category: {type: String, required: true},
 })
 
-const foodModel = mongoose.models.food ||  mongoose.model('Food', foodSchema)
+const foodModel = mongoose.model('Food', foodSchema)
 
 export default foodModel; 
 
