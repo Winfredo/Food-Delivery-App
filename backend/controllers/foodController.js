dd9e68b771f1e59277f69d7bb38d41261eaadc5b
winfredo <winfrednukpe2002@gmail.com>
2026-03-12 21:53:54 +0000

    for file upload

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,7 +1,25 @@
-import foodModel from "../models/foodModel";
 import fs from 'fs';
+import foodModel from '../models/foodModel.js';
 
- const createFood = async (req, res) => {
+ const createFood = async (req, res, next) => {
+     console.log("req.body:", req.body);  // Add this
+    console.log("req.file:", req.file);  // Add this
+    
+    let image_filename = req.file ? `${req.file.filename}` : null;
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: image_filename,
+        category: req.body.category
+    })
+    try {
+         await food.save();
+         res.status(201).json({ message: 'Food created successfully',success: true });
+    } catch (error) {
+        console.error('Error creating food:', error);
+        return next(error);
+    }
  }
 
  export { createFood }
