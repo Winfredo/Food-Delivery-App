32dfab5d5ca32e9566995cb6042b6eea7d5eab2c
winfredo <winfrednukpe2002@gmail.com>
2026-03-15 18:58:37 +0000

    fixes

--- admin/components/Navbar.tsx
+++ admin/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex items-center justify-between p-4'>
+    <div className='w-full flex items-center justify-between'>
       <img src="/assets/logo.png" alt='Logo'  />
       <img src="/assets/profile_image.png" alt='Profile picture' />
     </div>
