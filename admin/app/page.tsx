306211643c12bd6c2bd6020f3373b2de29d3ccbc
winfredo <winfrednukpe2002@gmail.com>
2026-03-16 23:16:26 +0000

    fixes

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,14 +1,15 @@
-import Navbar from '@/components/Navbar'
-import Sidebar from '@/components/Sidebar'
-import React from 'react'
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import React from "react";
 
 
 const page = () => {
   return (
-    <div className='text-center w-full'>
-      <p >Admin Dashboard</p>
-    </div>
-  )
-}
+    <div className="text-center w-full">
 
-export default page
+      <p>Admin Dashboard</p>
+    </div>
+  );
+};
+
+export default page;
