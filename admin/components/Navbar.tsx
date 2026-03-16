95f71ed69f5866a17029d5aa49b1f19f313ed05c
winfredo <winfrednukpe2002@gmail.com>
2026-03-16 16:38:57 +0000

    responsiveness fixes

--- admin/components/Navbar.tsx
+++ admin/components/Navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex items-center justify-between'>
-      <img src="/assets/logo.png" alt='Logo'  />
-      <img src="/assets/profile_image.png" alt='Profile picture' />
-    </div>
+   <div 
+  className='w-full h-20 flex items-center justify-between px-8 py-4 '>
+  <img src="/assets/logo.png" alt='Logo' className='w-20 md:w-[13%] ' />
+  <img src="/assets/profile_image.png" alt='Profile picture' className=' w-10' />
+</div>
   )
 }
 
