abb5dc25f631f6351e1651e6cd5a288e4535dc62
winfredo <winfrednukpe2002@gmail.com>
2026-03-18 18:16:51 +0000

    auth services.

---
+++ backend/services/user.service.js
@@ -0,0 +1,35 @@
+import userModel from "../models/userModel.js";
+import { hashPassword, comparePassword } from "../utils/auth.js";
+class UserService {
+  static async createUser({ name, email, password }) {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return null;
+    }
+    const hashedPassword = await hashPassword(password);
+
+    const user = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await user.save();
+    return savedUser;
+  }
+
+  static async loginUser({ email, password }) {
+    if (!email || !password) {
+      return null;
+    }
+    const user = await userModel.findOne({ email });
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      return null;
+    }
+
+    return user;
+  }
+}
+
+export default UserService;
