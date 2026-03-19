89a98f7989cf598ae96937386b8c0f7d321e804e
winfredo <winfrednukpe2002@gmail.com>
2026-03-19 19:02:59 +0000

    add new functions.

--- frontend/components/LoginPopup.tsx
+++ frontend/components/LoginPopup.tsx
@@ -1,11 +1,43 @@
-import React, { useState } from "react";
+"use client";
+import React, { useState, useContext } from "react";
 import { LoginPopupProps } from "@/types/type";
+import { storeContext } from "@/context/StoreContextProvider";
+import axios from "axios";
+ 
 
 const LoginPopup = ({ setShowLoginPopup }: LoginPopupProps) => {
   const [currentState, setCurrentState] = useState("Sign Up");
+  const {url,token, setToken} = useContext(storeContext)!;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+
+  const onLogin = async (e:any) => {
+    e.preventDefault();
+    let newUrl = url + (currentState === "Login" ? "/api/user/login" : "/api/user/signup");
+    
+    const response = await axios.post(newUrl, formData);
+    
+    if(response.data.success){
+      console.log(response.data);
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLoginPopup(false);
+    }else {
+      alert(response.data.message);
+    }
+  }
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((prev)=> ({...prev, [name]: value}))
+  }
   return (
     <div className="fixed inset-0 bg-[#00000090] bg-opacity-50 flex items-center justify-center z-50">
-      <form className="bg-white p-6 rounded-lg shadow-lg w-80">
+      <form onSubmit={onLogin} className="bg-white p-6 rounded-lg shadow-lg w-80">
   <div className="flex justify-between items-center mb-4">
     <h2 className="text-xl font-semibold text-gray-800">{currentState}</h2>
     <img
@@ -22,6 +54,9 @@ const LoginPopup = ({ setShowLoginPopup }: LoginPopupProps) => {
     ) : (
       <input 
         type="text" 
+        name="name"
+        value={formData.name}
+        onChange={onChangeHandler}
         placeholder="Your name" 
         required 
         className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-[#FF6347] focus:ring-1 focus:ring-[#FF6347] text-sm"
@@ -29,19 +64,25 @@ const LoginPopup = ({ setShowLoginPopup }: LoginPopupProps) => {
     )}
     <input 
       type="text" 
+      name="email"
+      value={formData.email}
+      onChange={onChangeHandler}
       placeholder="Your email" 
       required 
       className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-[#FF6347] focus:ring-1 focus:ring-[#FF6347] text-sm"
     />
     <input 
       type="password" 
+      name="password"
+      value={formData.password}
+      onChange={onChangeHandler}
       placeholder="Password" 
       required 
       className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-[#FF6347] focus:ring-1 focus:ring-[#FF6347] text-sm"
     />
   </div>
   
-  <button className="w-full mt-4 bg-[#FF6347] hover:bg-[#e5533d] text-white py-2 rounded transition-colors text-sm font-medium">
+  <button type="submit" className="w-full mt-4 bg-[#FF6347] hover:bg-[#e5533d] text-white py-2 rounded transition-colors text-sm font-medium">
     {currentState === "Sign Up" ? "Create Account" : "Login"}
   </button>
   
