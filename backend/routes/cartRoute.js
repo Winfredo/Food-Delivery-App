63c70d85f52926b125efa9554b7c801736c722ed
winfredo <winfrednukpe2002@gmail.com>
2026-03-21 22:56:04 +0000

    cart route

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addToCart, deleteFromCart, listCart } from "../controllers/cartController.js";
+import authMiddleware from "../middleware/auth.js";
 const router = express.Router();
 
-router.post('/add', addToCart);
-router.delete('/delete', deleteFromCart);
-router.get('/list', listCart);
+router.post('/add', authMiddleware, addToCart);
+router.delete('/delete', authMiddleware, deleteFromCart);
+router.get('/list', authMiddleware, listCart);
 
 export default router; 
