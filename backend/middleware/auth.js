05aefb8e0085a2cb7e0714e41ce42474e4e679ef
winfredo <winfrednukpe2002@gmail.com>
2026-03-22 22:39:06 +0000

    middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
 
+//auth middleware to verify the token sent by the client in the request header
 const authMiddleware = async (req, res, next) => {
-    
+
 }
 
 export default authMiddleware;
