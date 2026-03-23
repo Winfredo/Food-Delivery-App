437c731a4370b0f2df196f4697be29bb061d61a8
winfredo <winfrednukpe2002@gmail.com>
2026-03-23 21:41:43 +0000

    auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,19 @@ import jwt from 'jsonwebtoken';
 
 //auth middleware to verify the token sent by the client in the request header
 const authMiddleware = async (req, res, next) => {
-
+    const {token} = req.headers;
+    if(!token){
+        return res.status(401).json({success:false, message: 'Unauthorized.Login in again'});
+    }
+    try {
+      const token_decode = jwt.verify(token, process.env.JWT_SECRET);  
+        req.body = req.body || {};
+        req.body.userId = token_decode.id;
+        next();
+    }catch (error) {
+        console.error("Error in authMiddleware:", error); 
+        next(error);
+    }
 }
 
 export default authMiddleware;
