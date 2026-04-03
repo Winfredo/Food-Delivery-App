1c0a65f7db3fa6e19d9c95530023196ea43779ad
winfredo <winfrednukpe2002@gmail.com>
2026-04-03 23:23:27 +0000

    added verify order route.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder } from '../controllers/orderController.js';
+import { placeOrder,verifyOrder } from '../controllers/orderController.js';
 
 router.post('/place', authMiddleware, placeOrder);
-
+router.post('/verify', verifyOrder);
 
 export default router;
