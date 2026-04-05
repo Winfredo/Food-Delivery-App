34bc31e6b3b0a9c913a2ec2736d819f0e17f1a26
winfredo <winfrednukpe2002@gmail.com>
2026-04-05 04:34:46 +0000

    change the method.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder,verifyOrder } from '../controllers/orderController.js';
+import { placeOrder,verifyOrder,userOrder } from '../controllers/orderController.js';
 
 router.post('/place', authMiddleware, placeOrder);
 router.post('/verify', verifyOrder);
+router.get('/userorders', authMiddleware, userOrder);
 
 export default router;
