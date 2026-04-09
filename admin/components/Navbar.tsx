36ec8eb7234bcb238f27eb239ee025ec9f4c7ee8
winfredo <winfrednukpe2002@gmail.com>
2026-04-09 11:42:21 +0000

    ui fixes

--- admin/components/Navbar.tsx
+++ admin/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
    <div 
-  className='w-full h-20 flex items-center justify-between px-8 py-4 '>
+  className='w-full h-20 flex items-center justify-between px-8 py-7 mb-4 '>
   <img src="/assets/logo.png" alt='Logo' className='w-20 md:w-[13%] ' />
   <img src="/assets/profile_image.png" alt='Profile picture' className=' w-10' />
 </div>
