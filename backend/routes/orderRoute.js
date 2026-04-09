fde9a0bfbdb3bd6392349996fd933fcd63e323e6
winfredo <winfrednukpe2002@gmail.com>
2026-04-09 11:41:23 +0000

    list orders route.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder,verifyOrder,userOrder } from '../controllers/orderController.js';
+import { placeOrder,verifyOrder,userOrder,listOrders } from '../controllers/orderController.js';
 
 router.post('/place', authMiddleware, placeOrder);
 router.post('/verify', verifyOrder);
 router.get('/userorders', authMiddleware, userOrder);
+router.get('/list', listOrders);
 
 export default router;
