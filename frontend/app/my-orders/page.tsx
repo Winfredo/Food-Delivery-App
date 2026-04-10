c12a34e0d0028ef8c97510e6cced067b9c5f85c2
winfredo <winfrednukpe2002@gmail.com>
2026-04-10 01:50:21 +0000

    onclick for the track order button to update the status of the item.

--- frontend/app/my-orders/page.tsx
+++ frontend/app/my-orders/page.tsx
@@ -48,8 +48,8 @@ const page = () => {
                 </div>
                 <p className="text-base md:text-lg font-bold text-gray-800">Total: ${order.totalAmount % 1 === 0 ? `${order.totalAmount}.00` : order.totalAmount}</p>
                 <p className="text-gray-600 text-sm md:text-base font-bold ">Items: {order.items.length}</p>
-                <p className={`font-semibold text-sm md:text-base ${order.status === 'Food Processing' ? 'text-yellow-600' : order.status === 'Payment Successful' ? 'text-green-500' : 'text-blue-500'}`}>Status: {order.status}</p>
-                <button className="mt-2 w-full md:w-auto px-3 md:px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-sm md:text-base">Track Order</button>
+                <p className={`font-semibold text-sm md:text-base ${order.status === 'Food Processing' ? 'text-yellow-600' : order.status === 'Delivered' ? 'text-green-500' : 'text-blue-500'}`}>Status: {order.status}</p>
+                <button onClick={fetchOrders} className="mt-2 w-full md:w-auto px-3 md:px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-sm md:text-base">Track Order</button>
               </div>
               </div>
           )
