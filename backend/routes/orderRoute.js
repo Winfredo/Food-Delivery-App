6312ec6b7e62b41f93c30db909fd9beeaa4715c9
winfredo <winfrednukpe2002@gmail.com>
2026-04-10 01:49:41 +0000

    route for the status update

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder,verifyOrder,userOrder,listOrders } from '../controllers/orderController.js';
+import { placeOrder,verifyOrder,userOrder,listOrders,statusUpdate } from '../controllers/orderController.js';
 
 router.post('/place', authMiddleware, placeOrder);
 router.post('/verify', verifyOrder);
 router.get('/userorders', authMiddleware, userOrder);
 router.get('/list', listOrders);
+router.post('/status',statusUpdate)
 
 export default router;
