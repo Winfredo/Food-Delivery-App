b9676099fe07c7a48b18627f37e7143f5c67432b
winfredo <winfrednukpe2002@gmail.com>
2026-04-12 23:13:36 +0000

    retry payment route

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder,verifyOrder,userOrder,listOrders,statusUpdate } from '../controllers/orderController.js';
+import { placeOrder,verifyOrder,userOrder,listOrders,statusUpdate,retryPayment } from '../controllers/orderController.js';
 
 router.post('/place', authMiddleware, placeOrder);
 router.post('/verify', verifyOrder);
 router.get('/userorders', authMiddleware, userOrder);
 router.get('/list', listOrders);
 router.post('/status',statusUpdate)
+router.post('/retry', authMiddleware, retryPayment);
 
 export default router;
