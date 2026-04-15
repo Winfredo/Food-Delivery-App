59c4f45ba017df8a229b57dcf7d7da6b6eb86434
winfredo <winfrednukpe2002@gmail.com>
2026-04-15 23:25:03 +0000

    formatting

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,38 @@
-import express from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv';
-import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
 
-dotenv.config({quiet: true});
+dotenv.config({ quiet: true });
 
-import { connectDB } from './config/db.js';
-import foodRoute from './routes/foodRoute.js';
-import userRoute from './routes/userRoute.js';
-import cartRoute from './routes/cartRoute.js';
-import orderRoute from './routes/orderRoute.js';
+import { connectDB } from "./config/db.js";
+import foodRoute from "./routes/foodRoute.js";
+import userRoute from "./routes/userRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
 const app = express();
 const PORT = 4000;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 app.use(express.json());
 
 //db connection
 await connectDB();
 
-app.use('/api/food', foodRoute);
-app.use('/images', express.static('uploads'));
-app.use('/api/user', userRoute);
-app.use('/api/cart', cartRoute);
-app.use('/api/order', orderRoute);
+app.use("/api/food", foodRoute);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
 
-app.get("/", (req,res)=> {
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Server started on port ${PORT}`);
-    console.log('📚 Swagger docs available at http://localhost:4000/api-docs');
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+  console.log("Swagger docs available at http://localhost:4000/api-docs");
 });
